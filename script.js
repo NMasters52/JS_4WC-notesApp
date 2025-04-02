2b9fec8ca855d90e0ab793aedfdb12694f585237
Nicholas Masters <168689746+NMasters52@users.noreply.github.com>
2025-04-02 15:53:53 +0000

    add feat edit notes and save edits

--- script.js
+++ script.js
@@ -25,16 +25,52 @@ function saveNoteHandler() {
 function displayNotes() {
     elements.displayNotes.innerHTML ='';
     savedNotes.forEach(note => {
-      const  listItem = document.createElement('p')
+        const noteDiv = document.createElement('div');
+        elements.displayNotes.appendChild(noteDiv);
+        noteDiv.classList.add('note-item');
+        let  listItem = document.createElement('p')
         listItem.innerText = note.text;
-        elements.displayNotes.appendChild(listItem);
+        listItem.id = note.id;
+        noteDiv.appendChild(listItem);
+        let editBtn = document.createElement('button');
+        editBtn.innerText = "edit";
+        editBtn.addEventListener('click', () => editNote(note.id));
+        noteDiv.appendChild(editBtn);
     })
+}
+
+function editNote(noteId) {
+    const note = savedNotes.find(note => noteId === note.id)
+    if (note) {
+        try {
+           const listItem = document.getElementById(`${noteId}`)
+           let input = document.createElement('input');
+           input.value = note.text;
+           listItem.replaceWith(input);
+           const saveBtn = document.createElement('button')
+            saveBtn.innerText = "save";
+            saveBtn.addEventListener('click', () => editSaveHandler(input.value, noteId))
+            input.after(saveBtn)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        listItem.innerText = `${this.text}. edit failed.`;
+    }
+}
+
+function editSaveHandler(newText, noteId) {
+    const noteIndex = savedNotes.findIndex(note => noteId === note.id);
+    savedNotes[noteIndex].text = newText;
+    saveNotes();
+    displayNotes();
 }
 
 function saveNotes() {
     localStorage.setItem("notes", JSON.stringify(savedNotes));
 }
 
+//this is here to grab the notes and save them to the savedNotes array when the page loads
 function getNotes(){
   let storedNotes = localStorage.getItem("notes");
     if (storedNotes) {
